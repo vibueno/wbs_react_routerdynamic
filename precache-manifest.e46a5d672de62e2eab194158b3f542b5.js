c2d4c43a1791299b041ca14b04b04e48df44d5bf
vibueno <20514260+vibueno@users.noreply.github.com>
2021-01-12 15:28:41 +0100

    Updates

---
+++ precache-manifest.e46a5d672de62e2eab194158b3f542b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4cfa90a171bbd0534039bfd61943de1",
+    "url": "/wbs_react_routerdynamic/index.html"
+  },
+  {
+    "revision": "bfce3b0adc906cd858a1",
+    "url": "/wbs_react_routerdynamic/static/css/main.925daf99.chunk.css"
+  },
+  {
+    "revision": "f952c1f49f47c72d13b7",
+    "url": "/wbs_react_routerdynamic/static/js/2.5be048ae.chunk.js"
+  },
+  {
+    "revision": "bfce3b0adc906cd858a1",
+    "url": "/wbs_react_routerdynamic/static/js/main.e6684d0d.chunk.js"
+  },
+  {
+    "revision": "043f1cfc7123ea7bd026",
+    "url": "/wbs_react_routerdynamic/static/js/runtime~main.b9812ba6.js"
+  }
+]);
